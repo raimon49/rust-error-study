b711bfeb3e63035e8aeec635f719e91a6fea7535
raimon <raimon49@hotmail.com>
2020-07-26 10:08:51 +0900

    Add a little annotate comment

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,6 @@ fn main() {
         let _will_error_items = pirate_share(100, 0);
     });
 
-    let _err = result.as_ref().err(); // Result<T, E>から成功値/エラー値を借用しResult<&T, &E>として処理
+    let _err = result.as_ref().err(); // Result<T, E>から成功値/エラー値を借用しResult<&T, &E>として処理 ここではOption<&E>を取り出している
     assert!(result.is_err());
 }
